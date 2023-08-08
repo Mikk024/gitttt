b10284a3354e9ba4b476419f2d742e4cfa74aa07
Mikolaj Godek <mikolajgodek024@gmail.com>
2023-08-08 18:40:48 +0200

    add slash

--- config/app.php
+++ config/app.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\SanctumServiceProvider;
 
 return [
 
@@ -170,6 +171,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        SanctumServiceProvider::class,
     ])->toArray(),
 
     /*
